0ec0211d4169ee97e5cc4aa259fed5baaf29312c
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-10-21 13:35:32 +0200

    Create GetVersionCommand.cs

---
+++ Commands/GetVersionCommand.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RemoteControlV3.Commands
+{
+    [Command("Get-Version")]
+    public class GetVersionCommand : Command
+    {
+        public void Run(string[] args)
+        {
+        }
+    }
+}
